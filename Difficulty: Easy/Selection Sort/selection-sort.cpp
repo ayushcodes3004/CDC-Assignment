4f2150af2e313b64a76d8a57063c5ee65c3139c0
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-12-04 02:23:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Function to perform selection sort on the given array.
+    void selectionSort(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int minIdx=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[minIdx])  minIdx=j;
+            }
+            swap(arr[i],arr[minIdx]);
+        }
+    }
+};

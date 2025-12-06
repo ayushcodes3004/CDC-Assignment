efd0c654b935e2d18e1c180ea5565d8ac6446569
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-12-06 22:47:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.83%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -9,7 +9,12 @@ private:
 public:
     int climbStairs(int n) {
       if(n==1)  return 1;
-      vector<int>dp(n+1,-1);
-      return rec(0,n,dp); 
+      vector<int>dp(n+1,0);
+      dp[1]=1;
+      dp[2]=2;
+      for(int i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+      }
+      return dp[n]; 
     }
 };

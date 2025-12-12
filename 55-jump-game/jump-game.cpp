eafd408e49716a6e31f39d63d63a3dbedb945780
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-12-12 16:15:11 +0530

    Time: 270 ms (13.62%) | Memory: 53.2 MB (14.20%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -6,6 +6,7 @@ class Solution {
         int maxJumps=nums[i];
         bool ans=false;
         for(int j=1;j<=maxJumps;j++){
+            if(ans==true)   break;
             ans=ans||rec(i+j,nums,n,dp);
         }
         return dp[i]=ans;
@@ -13,7 +14,18 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return rec(0,nums,n,dp);
+        vector<bool>dp(n,0);
+        // return rec(0,nums,n,dp);
+        dp[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            int maxJumps=nums[i];
+            for(int j=1;j<=maxJumps;j++){
+                if(dp[i+j]){
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
 };

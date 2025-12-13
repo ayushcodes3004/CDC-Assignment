37fc54f5a0cdf422f54da1db2108bcf5da24ced5
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-12-13 10:59:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Most Frequent Character/most-frequent-character.cpp
+++ Difficulty: Easy/Most Frequent Character/most-frequent-character.cpp
@@ -2,21 +2,33 @@ class Solution {
   public:
     char getMaxOccuringChar(string& s) {
         //  code here
-        int freq[26]={0};
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            freq[s[i]-'a']++;
+        // int freq[26]={0};
+        // int n=s.size();
+        // for(int i=0;i<n;i++){
+        //     freq[s[i]-'a']++;
+        // }
+        // int maxi=0;
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]>maxi){
+        //         maxi=freq[i];
+        //     }
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]==maxi){
+        //         return i+'a';
+        //     }
+        // }
+        // return 'a';
+        map<char,int>freq;
+        for(char c: s){
+            freq[c]++;
         }
         int maxi=0;
-        for(int i=0;i<26;i++){
-            if(freq[i]>maxi){
-                maxi=freq[i];
-            }
+        for(auto [c,f]:freq){
+            maxi=max(maxi,f);
         }
-        for(int i=0;i<26;i++){
-            if(freq[i]==maxi){
-                return i+'a';
-            }
+        for(auto [c,f]:freq){
+            if(f==maxi) return c;
         }
         return 'a';
     }

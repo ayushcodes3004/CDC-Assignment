5e17828190175556beafcc6e45fa039287fdf400
Ayush Jaiswal <ayushcodes3004@gmail.com>
2025-12-28 06:02:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Heap Sort/heap-sort.cpp
+++ Difficulty: Medium/Heap Sort/heap-sort.cpp
@@ -1,19 +1,19 @@
 // The functions should be written in a way that array become sorted
 // in increasing order when heapSort() is called.
 
-class Solution {\
+class Solution {
   private:
-    void heapify(int i,int n,vector<int>& arr){
+    void heapify(vector<int>& arr, int n, int i){
         int largest=i;
         int left=2*i+1;
         int right=2*i+2;
-        
-        if(left<n && arr[left]>arr[largest])    largest=left;
-        if(right<n && arr[right]>arr[largest])  largest=right;
-        
+        if(left<n && arr[left]>arr[largest])
+            largest=left;
+        if(right<n && arr[right]>arr[largest])
+            largest=right;
         if(largest!=i){
             swap(arr[i],arr[largest]);
-            heapify(largest,n,arr);
+            heapify(arr,n,largest);
         }
     }
   public:
@@ -21,13 +21,12 @@ class Solution {\
     void heapSort(vector<int>& arr) {
         // code here
         int n=arr.size();
-        for(int i=n/2-1;i>=0;i--){
-            heapify(i,n,arr);
+        for(int i=n/2;i>=0;i--){
+            heapify(arr,n,i);
         }
-        
         for(int i=n-1;i>=0;i--){
             swap(arr[0],arr[i]);
-            heapify(0,i,arr);
+            heapify(arr,i,0);
         }
     }
 };
